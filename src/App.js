298bf926a81800b8d461ddbd684200dded8be4a8
B-Oz <bettysoftwareng@gmail.com>
2022-11-04 00:35:57 +0300

    mui instalization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route index element={<Main />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+const Main = () => {
+  return (
+    <>
+      <SearchBar />
+      <div className="cards">Cards</div>
+    </>
+  );
+};
+
+export default Main;
